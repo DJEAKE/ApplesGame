0ffb451bc10c83d051b1a924245f3403b5dd6436
DJEAKE <djeakee@gmail.com>
2023-09-16 22:05:48 +0800

    Added player movement & Game borders

--- ApplesGame/ApplesGame/GameMain.cpp
+++ ApplesGame/ApplesGame/GameMain.cpp
@@ -7,7 +7,7 @@
 const std::string RESOURCES_PATH = "Resources/";
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const float INITIAL_SPEED = 100;
+const float INITIAL_SPEED = 0.1f;
 const float PLAYER_SIZE = 20.f;
 
 int main()
@@ -17,6 +17,7 @@ int main()
 	float playerY = SCREEN_HEIGHT / 2.f;
 	float playerSpeed = INITIAL_SPEED;
 	int playerDirection = 0; // 0 - Right, 1 - Up, 2 - Left, 3 - down
+
 	sf::RectangleShape playerShape;
 	playerShape.setSize(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
 	playerShape.setFillColor(sf::Color::Red);
@@ -25,12 +26,58 @@ int main()
 
 	while (window.isOpen())
 	{
+		// Read events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		// Handle input
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			playerDirection = 0;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			playerDirection = 1;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			playerDirection = 2;
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			playerDirection = 3;
+		}
+
+		// Update player state
+		if(playerDirection == 0)
+		{
+			playerX += playerSpeed;
+		}
+		else if (playerDirection == 1)
+		{
+			playerY -= playerSpeed;
+		}
+		else if (playerDirection == 2)
+		{
+			playerX -= playerSpeed;
+		}
+		else if (playerDirection == 3)
+		{
+			playerY += playerSpeed;
+		}
+
+		// Check screen borders
+		if (playerX - PLAYER_SIZE / 2.f < 0.f || playerX + PLAYER_SIZE / 2.f > SCREEN_WIDTH ||
+			playerY - PLAYER_SIZE / 2.f < 0.f || playerY + PLAYER_SIZE / 2.f > SCREEN_HEIGHT)
+		{
+			window.close();
+			break;
+		}
+
 
 		window.clear();
 		playerShape.setPosition(playerX, playerY);
