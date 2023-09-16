15f43835032e7e98c8ea1264d7e187f20f67c9d2
DJEAKE <djeakee@gmail.com>
2023-09-16 20:17:06 +0800

    Created Solution

---
+++ ApplesGame/ApplesGame/GameMain.cpp
@@ -0,0 +1,42 @@
+﻿// ©2023, XYZ School. All rights reserved.
+// Authored by Aleksandr Rybalka (polterageist@gmail.com)
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+const std::string RESOURCES_PATH = "Resources/";
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const float INITIAL_SPEED = 100;
+const float PLAYER_SIZE = 20.f;
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Apples game by DJEAKE <3");
+	float playerX = SCREEN_WIDTH / 2.f;
+	float playerY = SCREEN_HEIGHT / 2.f;
+	float playerSpeed = INITIAL_SPEED;
+	int playerDirection = 0; // 0 - Right, 1 - Up, 2 - Left, 3 - down
+	sf::RectangleShape playerShape;
+	playerShape.setSize(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
+	playerShape.setFillColor(sf::Color::Red);
+	playerShape.setOrigin(PLAYER_SIZE / 2.f, PLAYER_SIZE / 2.f);
+	playerShape.setPosition(playerX, playerY);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		playerShape.setPosition(playerX, playerY);
+		window.draw(playerShape);
+		window.display();
+	}
+
+	return 0;
+}
