a4be73a84751e5ac330c7bdbe2e8055a70883f9b
DJEAKE <djeakee@gmail.com>
2023-09-17 12:57:41 +0800

    Added apples generation

--- ApplesGame/ApplesGame/GameMain.cpp
+++ ApplesGame/ApplesGame/GameMain.cpp
@@ -10,29 +10,63 @@ const int SCREEN_HEIGHT = 600;
 const float INITIAL_SPEED = 100.f;
 const float PLAYER_SIZE = 20.f;
 const float ACCELERATION = 20.f;
+const int NUM_APPLES = 20;
+const float APPLE_SIZE = 20.f;
 
 int main()
 {
+	// Init seed for radom
+	int seed = time(nullptr);
+	srand(seed);
+
+	// Init Window
 	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Apples game by DJEAKE <3");
+	
+	// Init player state
 	float playerX = SCREEN_WIDTH / 2.f;
 	float playerY = SCREEN_HEIGHT / 2.f;
 	float playerSpeed = INITIAL_SPEED;
 	int playerDirection = 0; // 0 - Right, 1 - Up, 2 - Left, 3 - down
 
+	// Init player shape
 	sf::RectangleShape playerShape;
 	playerShape.setSize(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
 	playerShape.setFillColor(sf::Color::Red);
 	playerShape.setOrigin(PLAYER_SIZE / 2.f, PLAYER_SIZE / 2.f);
 	playerShape.setPosition(playerX, playerY);
 
-	sf::Clock gameCloak;
-	float lastTime = gameCloak.getElapsedTime().asSeconds();
+	// Init apple
+	float applesX[NUM_APPLES];
+	float applesY[NUM_APPLES];
+	bool isAppleEaten[NUM_APPLES];
+	sf::RectangleShape appleShape[NUM_APPLES];
+	
+	for (int i = 0; i < NUM_APPLES; ++i)
+	{
+		// Init apple state
+		isAppleEaten[i] = false;
+		applesX[i] = rand() / (float) RAND_MAX * SCREEN_WIDTH;
+		applesY[i] = rand() / (float) RAND_MAX * SCREEN_HEIGHT;
 
+		// Init apple shape
+		appleShape[i].setSize(sf::Vector2f(APPLE_SIZE, APPLE_SIZE));
+		appleShape[i].setFillColor(sf::Color::Green);
+		appleShape[i].setOrigin(APPLE_SIZE / 2.f, APPLE_SIZE / 2.f);
+		appleShape[i].setPosition(applesX[i], applesY[i]);
+	}
+
+	// Init game clock
+	sf::Clock gameClock;
+	float lastTime = gameClock.getElapsedTime().asSeconds();
+
+	// Main Loop
 	while (window.isOpen())
 	{
-		float currentTime = gameCloak.getElapsedTime().asSeconds();
+		// Calculate time delta
+		float currentTime = gameClock.getElapsedTime().asSeconds();
 		float deltaTime = currentTime - lastTime;
 		lastTime = currentTime;
+
 		// Read events
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -60,6 +94,7 @@ int main()
 		}
 
 		playerSpeed += ACCELERATION * deltaTime;
+
 		// Update player state
 		if(playerDirection == 0)
 		{
@@ -89,6 +124,13 @@ int main()
 
 		window.clear();
 		playerShape.setPosition(playerX, playerY);
+		for (int i = 0; i < NUM_APPLES; ++i)
+		{
+			if (!isAppleEaten[i])
+			{
+				window.draw(appleShape[i]);
+			}
+		}
 		window.draw(playerShape);
 		window.display();
 	}
